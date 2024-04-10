a1225952818d849e9b0e16d06fdf0720106431db
cubem4st3r <benjaminfinck02@googlemail.com>
2024-04-10 05:34:37 +0200

    fix: added item

--- src/Classes/VislandRouteItem.py
+++ src/Classes/VislandRouteItem.py
@@ -1,21 +1,15 @@
+from Classes.Item import Item
+
 class VislandRouteItem:
-    def __init__(self, data: dict = None):
-        if data is not None:
-            self._id = data.get("id")
-            self._route = data.get("route")
-            self._item = data.get("item")
+    def __init__(self, item: Item = None):
+        if item is not None:
+            self._item = item
         else:
-            self._id = None
-            self._route = None
             self._item = None
 
     @property
     def id(self):
         return self._id
-    
-    @property
-    def route(self):
-        return self._route
     
     @property
     def item(self):
@@ -24,10 +18,6 @@ class VislandRouteItem:
     @id.setter
     def id(self, value):
         self._id = value
-
-    @route.setter
-    def route(self, value):
-        self._route = value
 
     @item.setter
     def item(self, value):
